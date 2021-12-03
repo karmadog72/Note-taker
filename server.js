9fe58da0ef3fc8ff78f5f2b64de834cba0380b8f
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-03 15:57:46 -0700

    added express middleware

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const fs = require("fs");
-const filePath = require("filePath");
+const path = require("path");
+const database = require("./db/db.json");
 
 //instantiate the server
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.listen(3000, () => {
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/notes", (req, res) => {
+  res.json(allNotes.slice(1));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public.index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public.notes.html"));
+});
+
+app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
 });
