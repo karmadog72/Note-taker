887281830166244f90b34da5f6f0d4b23d888cb0
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-03 19:37:46 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/api/notes", (req, res) => {
-  res.json(allNotes.slice(1));
+  res.json(database.slice(1));
 });
 
 app.get("/", (req, res) => {
@@ -24,31 +24,47 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public.index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./public.index.html"));
+});
 
 function generateNote(body, notesArray) {
-    var note = body;
-    if (!Array.isArray(notesArray) {
-        notesArray = [];
-        if (notesArray.length === 0)
-        notesArray.push(0);
-        body.id = notesArray [0];
-        notesArray[0]++;
-        notesArray.push(note);
-        fs.writeFileSync(
-            path.join(__dirname, './db/db.json'),
-            JSON.stringify(notesArray, null, 2)
-        );
-        return note;
+  var note = body;
+  if (!Array.isArray(notesArray)) notesArray = [];
+  if (notesArray.length === 0) notesArray.push(0);
+  body.id = notesArray[0];
+  notesArray[0]++;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return note;
+}
+app.post("api/notes", (req, res) => {
+  const note = generateNote(req.body, database);
+  res.json(note);
+});
+
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+
+    if (note.id === id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+      break;
     }
-    app.post('api/notes', (req, res) => {
-        const note = generateNote(req.body, allNotes);
-        res.json(note);
-    });
-    
+  }
+}
 
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNote(req.params.id, database);
+  res.JSON(true);
+});
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
