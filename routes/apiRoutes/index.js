aa27d43e5947c872bc67444ed51f15aa7a691986
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-05 16:01:36 -0700

    added routes index.js & post, pushdata

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,24 +1,27 @@
 const router = require("express").Router();
+const fs = require("fs");
+// const database = fs.readFileSync("", "utf8");
+let database = require("../../db/db.json");
+const path = require("path");
+// router.use(note.js);
 
-router.use(note.js);
-
-app.get("/api/notes", (req, res) => {
-  res.json(database.slice(1));
+router.get("/notes", (req, res) => {
+  return res.json(database);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+router.post("/notes", (req, res) => {
+  // req.body <-- comes from request from the front end
+  const noteBody = req.body;
+  let newData = database || [];
+  newData.push(noteBody);
+  // fs.writeFile as db.json and make the file contents newData
+  fs.write("newData", db.json(database), (err) => {
+    if (err) throw err;
+  });
+  console.log(noteBody);
 });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.delete("/api/notes/:id", (req, res) => {
+router.delete("/api/notes/:id", (req, res) => {
   deleteNote(req.params.id, database);
   res.JSON(true);
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const apiRoutes = require("./routes/apiRoutes/index");
+const htmlRoutes = require("./routes/index");
 
 //instantiate the server
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
